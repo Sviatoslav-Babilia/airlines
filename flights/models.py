ff7f42e05e219a10cb7b923c5ddd202cb6e02ff5
Sviatoslav Babilia <babilja@gmail.com>
2022-01-01 21:32:55 +0200

    Init

---
+++ flights/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Flight(models.Model):
+    origin = models.CharField(max_length=60)
+    destination = models.CharField(max_length=60)
+    duration = models.IntegerField()
