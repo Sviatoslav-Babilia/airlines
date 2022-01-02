6079c13826b97be1476f5ead96709bb65b2c4f7c
Sviatoslav Babilia <babilja@gmail.com>
2022-01-02 20:59:01 +0200

    Add a Airport model

--- flights/models.py
+++ flights/models.py
@@ -1,8 +1,21 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
 
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
+
+
 class Flight(models.Model):
-    origin = models.CharField(max_length=60)
-    destination = models.CharField(max_length=60)
+    origin = ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
