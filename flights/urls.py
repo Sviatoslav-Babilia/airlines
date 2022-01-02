3e1959939496df715d1ceecfd7b7f01c143d1e2e
Sviatoslav Babilia <babilja@gmail.com>
2022-01-02 21:16:10 +0200

    Add a index page to flights

--- flights/urls.py
+++ flights/urls.py
@@ -1,6 +1,7 @@
+from typing import ValuesView
 from django.urls import path
 from . import views
 
 urlpatterns =[
-    
+    path("", views.index, name='index')
 ]
--- flights/views.py
+++ flights/views.py
@@ -7,5 +7,4 @@ from .models import Flight, Airport
 def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
-    }
-    )
+    })
