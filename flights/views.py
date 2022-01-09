061d2759bb3c42290eee746de5e78eeb09ad3c40
Sviatoslav Babilia <babilja@gmail.com>
2022-01-09 17:22:33 +0200

    Add a passenger registration

--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,8 @@
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-from .models import Flight, Airport
+from .models import Flight, Airport, Passenger
 
 # Create your views here.
 
@@ -10,8 +12,19 @@ def index(request):
     })
 
 def flight(request, flight_id):
-    flight = Flight.objects.get(pk=flight_id)
+    flight = Flight.objects.get(id=flight_id)
+    passengers = flight.passengers.all()
     return render(request, "flights/flight.html", {
         "flight": flight,
-        "passengers": flight.passengers.all()
+        "passengers": passengers,
+        "non_passengers": Passenger.objects.exclude(fligths=flight).all()
     })
+
+def book(request, flight_id):
+
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger_id = int(request.POST["passenger"])
+        passenger = Passenger.objects.get(pk=passenger_id)
+        passenger.fligths.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
